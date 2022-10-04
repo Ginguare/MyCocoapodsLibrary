966313a2ae405790b433cc777c97b51ec9193754
Ginguare <vladik.pedko@gmail.com>
2022-10-04 20:44:45 +0300

    pod

---
+++ MyCocoapodsLibrary/Classes/Logger.swift
@@ -0,0 +1,21 @@
+//
+//  Logger.swift
+//  MyCocoapodsLibrary
+//
+//  Created by Влад Педько on 01.10.2022.
+//
+
+import Foundation
+
+public class Logger {
+    
+    public init(){}
+    
+     public func printLog() {
+        print("Hello, World!")
+    }
+    
+    func privateMethod() {
+        print("private")
+    }
+}
